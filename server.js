f3f6920444216f01c7127f5a1659c539a2e66054
Sergio <sergio@Sergios-MBP.home>
2020-02-24 13:27:06 -0500

    created simple CRUD operations that return a success message

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ dotenv.config({ path: './config/config.env' })
 
 const app = express()
 
+app.get('/api/v1/bootcamps', (req, res) => {
+	return res.status(200).json({ sucess: 'true', msg: 'show all bootcamps' })
+})
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+	return res.status(200).json({ sucess: 'true', msg: `show bootcamp ${req.params.id}` })
+})
+
+app.post('/api/v1/bootcamps', (req, res) => {
+	return res.status(200).json({ sucess: 'true', msg: 'create new bootcamp' })
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+	return res.status(200).json({ sucess: 'true', msg: `update bootcamp ${req.params.id}` })
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+	return res.status(200).json({ sucess: 'true', msg: `delete bootcamp ${req.params.id}` })
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
