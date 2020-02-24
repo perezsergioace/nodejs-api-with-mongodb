d67baa7dd055edefac899cc0d1d04fe2bae40e46
Sergio <sergio@Sergios-MBP.home>
2020-02-24 17:48:21 -0500

    added colors library to highlight successful and error mesages in the terminal

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const connectDB = require('./config/db')
+const colors = require('colors')
 
 // Load env vars
 dotenv.config({ path: './config/config.env' })
@@ -24,11 +25,11 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-const server = app.listen(PORT, console.log(`*** Server running in ${process.env.NODE_ENV} mode on port ${PORT} ***`))
+const server = app.listen(PORT, console.log(`*** Server running in ${process.env.NODE_ENV} mode on port ${PORT} ***`.yellow.bold))
 
 // Handle ungandled promise rejections
 process.on('unhandledRejection', (error, promise) => {
-    console.log(`Error: ${error.message}`)
+    console.log(`Error: ${error.message}`.red)
     // Close server & exit process
     server.close(() => process.exit(1))
 })
