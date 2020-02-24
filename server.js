1e8a0a9e39465d0d81af53d365c6ac17e7e7c6de
Sergio <sergio@Sergios-MBP.home>
2020-02-24 16:43:29 -0500

    Connected to the database with mongoose

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const connectDB = require('./config/db')
 
 // Load env vars
 dotenv.config({ path: './config/config.env' })
+
+// Connect to database
+connectDB()
 
 // Route files
 const bootcamps = require('./routes/bootcamps')
@@ -20,4 +24,11 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`*** Server running in ${process.env.NODE_ENV} mode on port ${PORT} ***`))
+const server = app.listen(PORT, console.log(`*** Server running in ${process.env.NODE_ENV} mode on port ${PORT} ***`))
+
+// Handle ungandled promise rejections
+process.on('unhandledRejection', (error, promise) => {
+    console.log(`Error: ${error.message}`)
+    // Close server & exit process
+    server.close(() => process.exit(1))
+})
