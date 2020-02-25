50d57661c919a982019ec2f50b7bfd7709486f17
Sergio Perez <perez.sergio.ace@gmail.com>
2020-02-24 20:03:53 -0500

    added GET functionality to the API. User is able to get all the bootcamps in the database and also get a single bootcamp based on the bootcamp's id

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,31 @@ const Bootcamp = require('../models/Bootcamp')
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-	return res.status(200).json({ sucess: true, msg: 'show all bootcamps' })
+exports.getBootcamps = async (req, res, next) => {
+	try {
+		const bootcamps = await Bootcamp.find()
+
+		res.status(200).json({ success: true, data: bootcamps })
+	} catch (error) {
+		res.status(400).json({ success: false })
+	}
 }
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-	return res.status(200).json({ sucess: true, msg: `show bootcamp ${req.params.id}` })
+exports.getBootcamp = async (req, res, next) => {
+	try {
+		const bootcamp = await Bootcamp.findById(req.params.id)
+
+		if (!bootcamp) {
+			return res.status(400).json({ success: false })
+		}
+
+		res.status(200).json({ success: true, data: bootcamp })
+	} catch (error) {
+		res.status(400).json({ success: false })
+	}
 }
 
 // @desc    Create new bootcamp bootcamp
@@ -43,5 +59,3 @@ exports.updateBootcamp = (req, res, next) => {
 exports.deleteBootcamp = (req, res, next) => {
 	return res.status(200).json({ sucess: true, msg: `delete bootcamp ${req.params.id}` })
 }
-// quick test to see if my commits are working properly again - ignore this just a quick test
-
