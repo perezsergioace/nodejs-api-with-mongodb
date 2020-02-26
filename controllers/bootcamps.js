7e6650107bc18e8189a7a33c611926a9089e874f
Sergio Perez <perez.sergio.ace@gmail.com>
2020-02-25 20:18:12 -0500

    added advanced filtering. A user can insertfilters in the url and the bootcamp(s) that match will show up in the output result

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,7 +7,15 @@ const ErrorResponse = require('../utils/errorResponse')
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-	const bootcamps = await Bootcamp.find()
+	let query
+
+	let queryString = JSON.stringify(req.query)
+
+	queryString = queryString.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`)
+
+	query = Bootcamp.find(JSON.parse(queryString))
+
+	const bootcamps = await query
 
 	res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
 })
