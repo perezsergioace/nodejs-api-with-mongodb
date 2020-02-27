e7cdf99ae85aac6a3ad7baa245db3fccc91e148b
Sergio Perez <perez.sergio.ace@gmail.com>
2020-02-27 13:17:40 -0500

    In the bootcamps route file added the router for the course. Also added the ability to re-route into the courses route.

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,4 +1,5 @@
 const express = require('express')
+
 const {
 	getBootcamps,
 	getBootcamp,
@@ -7,7 +8,14 @@ const {
 	deleteBootcamp,
 	getBootcampInRadius
 } = require('../controllers/bootcamps')
+
+// Include other resource routers
+const courseRouter = require('./courses')
+
 const router = express.Router()
+
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter)
 
 router.route('/radius/:zipcode/:distance').get(getBootcampInRadius)
 
--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,8 @@
 const express = require('express')
-const {
-	getCourses,
-} = require('../controllers/courses')
 
-const router = express.Router()
+const { getCourses } = require('../controllers/courses')
+
+const router = express.Router({ mergeParams: true })
 
 router.route('/').get(getCourses)
 
